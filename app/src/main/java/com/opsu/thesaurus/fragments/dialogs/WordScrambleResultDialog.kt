42fafcea33d0a26e4e9d8b6b1e44ff2143b06947
Daniil Kyselov <dan.kiselev02@gmail.com>
2022-06-16 19:25:28 +0300

    finished world scramble

--- app/src/main/java/com/opsu/thesaurus/fragments/dialogs/WordScrambleResultDialog.kt
+++
@@ -1,4 +0,0 @@
-package com.opsu.thesaurus.fragments.dialogs
-
-class WordScrambleResultDialog {
-}
--- app/src/main/java/com/opsu/thesaurus/game_activities/WordScrambleActivity.kt
+++ app/src/main/java/com/opsu/thesaurus/game_activities/WordScrambleActivity.kt
@@ -2,67 +2,94 @@ package com.opsu.thesaurus.game_activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.opsu.thesaurus.R
 import com.opsu.thesaurus.database.entities.Entities
 import com.opsu.thesaurus.databinding.ActivityWordScrambleBinding
+import com.opsu.thesaurus.fragments.dialogs.GameCorrectAnswerDialog
+import com.opsu.thesaurus.fragments.dialogs.GameWrongAnswerDialog
 import kotlin.random.Random
 
 class WordScrambleActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityWordScrambleBinding
-    var maxAttemtps : Int = 3;
+    private var currentIteration = 0
+    private var correctAnswers = 0
+    private var currentWord: String = ""
+    private var terms: ArrayList<Entities.Term> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
             super.onCreate(savedInstanceState)
             binding = ActivityWordScrambleBinding.inflate(layoutInflater)
             setContentView(binding.root)
-            // початок гри
+
+            terms = intent.extras?.getParcelableArrayList<Entities.Term>("terms") as ArrayList<Entities.Term>
+            terms.shuffle()
+
+            binding.toolbar.toolbarCancel.setOnClickListener {
+                onBackPressed()
+            }
+            binding.toolbar.txtGameTitle.text = resources.getString(R.string.card_game2)
+            binding.toolbar.imgGameTitle.setImageResource(R.drawable.ic_wordscramble)
+
+            binding.toolbar.progressIndicator.apply {
+                this.max = terms.size
+                this.progress = 0
+            }
+
             startGame()
         }
 
-    // функція, яка підготовує дане Activity до початку гри
+
     private fun startGame() {
-        var currentAttempts = 0;
+
         // завантаження слова з ресурсів
-        val sWord = getWord()
+        val sWord = terms[currentIteration].term
+
         // перемішування завантаженого слова
         val mixedWord = mixWord(sWord)
         binding.scrambledWord.text = mixedWord
 
         binding.buttonUnscramble.setOnClickListener {
             val userWord = binding.userInput.text.toString() ?: ""
-            if (userWord.uppercase() == sWord.uppercase()) {
-                Toast.makeText(this, "You guessed!", Toast.LENGTH_SHORT).show()
+            if (userWord.lowercase() == sWord.lowercase()) {
+                correctAnswers+=1
+                val dialog = GameCorrectAnswerDialog()
+                dialog.show(supportFragmentManager, "choiceResult")
                 binding.userInput.text.clear()
-                startGame()
+                binding.toolbar.progressIndicator.progress += 1
+                checkForFinish()
             }
             else {
-                Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show()
-                currentAttempts += 1;
+                val dialog = GameWrongAnswerDialog(sWord, sWord, userWord)
+                dialog.show(supportFragmentManager, "choiceResult")
+                binding.userInput.text.clear()
+                binding.toolbar.progressIndicator.progress += 1
+                checkForFinish()
             }
+        }
+        currentIteration += 1
+    }
+
+    private fun checkForFinish() {
+        if (currentIteration != terms.size) {
+            startGame()
+        } else {
+            binding.buttonUnscramble.isClickable = false
         }
     }
 
-    // фукнкція для отримання випадкового слова з масиву рядків
-    private fun getWord(): String {
-
-        val terms =
-            intent.extras?.getParcelableArrayList<Entities.Term>("terms") as ArrayList<Entities.Term>
-
-        return terms[Random.nextInt(terms.size)].term
-    }
-
-    // функція для перемішування рядку
     private fun mixWord(word: String): String {
 
         val shuffledWord = word.toCharArray().let {
             it.shuffle()
             it.concatToString()
         }
-
         return shuffledWord
     }
+
 
 }
 
